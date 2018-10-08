bf776238e04973b446995ddeba4bca08ab3acc94
miknhoj <kimjohan86@gmail.com>
2018-10-08 18:03:27 -0400

    add home component - react renders home

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Welcome from './components/Welcome'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path ='/' component={Welcome} /> 
+          <Route exact path ='/home' component={Home} />
         </Switch>
       </Router>
     );
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-          <h3>Welcome to</h3>
-          <h1>Cigar.log</h1>
-          <Link to ='/home'>Click</Link>
+        Home
       </div>
     )
   }
