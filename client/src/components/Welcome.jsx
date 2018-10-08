1c1a1c5f4e9d8c7df981557c442991aea71223f4
miknhoj <kimjohan86@gmail.com>
2018-10-08 19:03:09 -0400

    made image a link to home page from welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,7 +7,8 @@ export default class Home extends Component {
       <div>
           <h3>Welcome to</h3>
           <h1>Cigar.log</h1>
-          <Link to ='/home'>Click</Link>
+          <Link to ='/home'><img src="https://ubisafe.org/images/fire-svg-1.png" alt={'have to be'}/></Link>
+          
       </div>
     )
   }
