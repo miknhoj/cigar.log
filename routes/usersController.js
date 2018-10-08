d4f9f84a8fa4c20f6d5a8edc7b1ae0fd29fd737b
miknhoj <kimjohan86@gmail.com>
2018-10-08 16:40:25 -0400

    get user by id route added

--- routes/usersController.js
+++ routes/usersController.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../db/model')
 
-/* GET users listing. */
+//SHOW ALL
 router.get('/', async function(req, res) {
   const users = await User.find()
   res.send(users);
 });
+
+//SHOW ONE
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id)
+  res.send(user)
+})
 
 module.exports = router;
