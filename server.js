eaf201f9b4ed56685e4dbee42c5993ae1a37334b
miknhoj <kimjohan86@gmail.com>
2018-10-08 15:09:02 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+// CONNECTING DATABASE
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+const connection = mongoose.connection;
+connection.on('connected', () => {
+    console.log('Mongoose Connected Successfully');
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client/build/index.html')));
+
+app.get('/', (req, res) => {res.sendFile(__dirname + '/client/build/index.html')})
+
+
+module.exports = app;
+
