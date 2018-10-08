c58609128056b43f761b6a253f42bae4f301e994
miknhoj <kimjohan86@gmail.com>
2018-10-08 17:50:26 -0400

    added welcome page

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+          <h3>Welcome to</h3>
+          <h1>Cigar.log</h1>
+          <div>Click</div>
+      </div>
+    )
+  }
+}
