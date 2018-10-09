08eb0c26d9aa81c2e06bae2ee304f5c293c7739f
miknhoj <kimjohan86@gmail.com>
2018-10-09 11:16:55 -0400

    react - add new user route added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom'
 export default class Home extends Component {
     state = {
         users:[],
+        newUser: {
+            userName:''
+        }
     }
 
 componentDidMount = async () => {
@@ -12,6 +15,22 @@ componentDidMount = async () => {
     console.log('Home component', response)
     this.setState({ users: response.data })
 }
+
+handleChange = (e) => {
+    const newUser = {...this.state.newUser}
+    newUser[e.target.name] = e.target.value
+    this.setState({ newUser })
+}
+
+handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await axios.post('/api/users', this.state.newUser)
+    const users = [...this.state.users]
+    users.push(response.data)
+    this.setState({users})
+}
+
+
 
   render() {
       const usersList = this.state.users.map((user, i) => {
@@ -27,6 +46,11 @@ componentDidMount = async () => {
       <div>
         Home
         {usersList}
+        <form onSubmit={this.handleSubmit}>
+            <input type='text' name='userName' value={this.state.newUser.userName} onChange={this.handleChange}/>
+            <input type='submit' value='Create New User' />
+        </form>
+
       </div>
     )
   }
