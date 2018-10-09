8c9a53be96e973ad35ca15c1527a71749b80eb0f
miknhoj <kimjohan86@gmail.com>
2018-10-09 11:48:37 -0400

    react - delete route added for user

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export default class User extends Component {
     state = {
@@ -21,10 +22,20 @@ export default class User extends Component {
         this.getUser()
     ]
 
+    handleDelete = async () => {
+        const userId = this.props.match.params.userId
+        await axios.delete(`/api/users/${userId}`)
+        this.setState({ redirect: true })
+    }
+
   render() {
+      if (this.state.redirect){
+          return <Redirect to='/home'/>
+      }
       const cigarsList = this.state.cigars.map((cigar, i) => {
           return <div key={i}>
             {cigar.cigarName}
+
           </div>
       })
     return (
@@ -32,6 +43,7 @@ export default class User extends Component {
           Single User 
           <h1>{this.state.user.userName}</h1>
           {cigarsList}
+          <button onClick={() => this.handleDelete()}>Delete User</button>
       </div>
     )
   }
